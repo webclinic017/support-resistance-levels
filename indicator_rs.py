d6544cefbd2a74e1e1de69a1a4a49da51b514c66
Khmyrov Igor <khmyrov.iv@gmail.com>
2021-05-30 17:54:48 +0300

    Fix null level array

--- indicator_rs.py
+++ indicator_rs.py
@@ -15,7 +15,7 @@ def convert_to_df(data):
     # Process of preparing data
     rowData = pd.DataFrame(columns=['Datetime', 'Open', 'High', 'Low', 'Close', 'Volume'])
     for i, candle in enumerate(data):
-        print(i)
+        # print(i)
         rowData = rowData.append({
             'Datetime': data.datetime.date(i),
             'Open': data.open[i],
@@ -46,24 +46,32 @@ class RSIndricator(bt.Indicator):
 
     def __init__(self):
         # self.csv_data = pd.read_csv('datas/orcl-1995-2014.txt')
-        self.clsp = RawPriceClusterLevels(None, merge_percent=0.25, use_maximums=True, bars_for_peak=91)
-        self.clrs = RawPriceClusterLevels(None, merge_percent=0.25, use_maximums=False, bars_for_peak=91)
+        self.clsp = RawPriceClusterLevels(None, merge_percent=0.7, use_maximums=True, bars_for_peak=21)
+        self.clrs = RawPriceClusterLevels(None, merge_percent=0.7, use_maximums=False, bars_for_peak=21)
+        print('support resistance cnt')
+        self.cnt = 0
+        self.supportVal = 0
+        self.resistVal = 0
+        #self.lines.support[0] = 0
+        #self.lines.resistance[0] = 0
 
     def next(self):
         df_data = convert_to_df(self.datas[0])
         self.clsp.fit(df_data)
         self.clrs.fit(df_data)
-
+        self.cnt += 1
         if self.datas[0].close[-1] <= self.datas[0].open[-1]:
             highPrice = self.datas[0].open[-1]
             lowPrice = self.datas[0].close[-1]
         else:
             highPrice = self.datas[0].close[-1]
             lowPrice = self.datas[0].open[-1]
+        if not(self.clsp.levels is None or self.clrs.levels is None):
+            self.supportVal = min(v['price'] for v in self.clsp.levels
+                                     if v['price'] >= highPrice)  # TODO: has to be redefined using RawPriceClusterLevels
+            self.resistVal = max(v['price'] for v in self.clrs.levels
+                                        if v['price'] <= lowPrice)  # TODO: has to be redefined using RawPriceClusterLevels
 
-        self.lines.support[0] = min(v['price'] for v in self.clsp.levels if
-                                    v['price'] >= highPrice)  # TODO: has to be redefined using RawPriceClusterLevels
-        self.lines.resistance[0] = max(v['price'] for v in self.clrs.levels if
-                                       v['price'] <= lowPrice)  # TODO: has to be redefined using RawPriceClusterLevels
-        # print(self.lines.support[0])
-        # print(self.lines.resistance[0])
+        self.lines.support[0] = self.supportVal
+        self.lines.resistance[0] = self.resistVal
+        print(str(self.lines.support[0]) + ' ' + str(self.lines.resistance[0]) + ' ' + str(self.cnt))
